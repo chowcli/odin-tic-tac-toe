99c9bd1651c5fe2dc1886fac174a5ace3189c78f
chowcli <136085102+chowcli@users.noreply.github.com>
2023-09-01 21:06:40 +0700

    Display marker on the screen, still need to implement check winner for game logic

---
+++ index.js
@@ -0,0 +1,62 @@
+const gameBoard = (() => {
+  const board = ["", "", "", "", "", "", "", "", ""];
+
+  const insertMark = (index, marker) => (board[index] = marker);
+  const getValueOfIndex = index => board[index];
+
+  // Reset Board
+  const resetBoard = () => {
+    for (let i = 0; i < board.length; i++) {
+      board[i] = "";
+    }
+  };
+
+  return { insertMark, getValueOfIndex, resetBoard };
+})();
+
+const gameController = (() => {
+  const mainContainer = document.querySelector(".main-container");
+
+  mainContainer.addEventListener("click", event => {
+    const { target } = event;
+
+    if (target.closest(".cell") && !target.textContent) {
+      const cellIndex = target.dataset.index;
+      const currMarker = roundSetUp.getCurrRoundMarker();
+      gameBoard.insertMark(cellIndex, currMarker);
+
+      target.textContent = gameBoard.getValueOfIndex(cellIndex);
+      roundSetUp.countRound();
+    }
+  });
+})();
+
+// Create player object
+const playerFactory = marker => {
+  const getMarker = () => marker;
+
+  return { getMarker };
+};
+
+const initializePlayer = (() => {
+  const player1 = playerFactory("X");
+  const player2 = playerFactory("O");
+
+  return { player1, player2 };
+})();
+
+// Round management logic
+const roundSetUp = (() => {
+  let round = 1;
+
+  const countRound = () => round++;
+
+  const getCurrRoundMarker = () => {
+    const { player1, player2 } = initializePlayer;
+    return round % 2 === 1 ? player1.getMarker() : player2.getMarker();
+  };
+
+  const resetRound = () => (round = 1);
+
+  return { countRound, getCurrRoundMarker, resetRound };
+})();
